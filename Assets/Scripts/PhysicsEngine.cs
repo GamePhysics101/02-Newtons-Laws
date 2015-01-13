a2655511f3d9fba023b64f821aee871645f5c034
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-13 10:56:26 +0000

    GP02.02 - Newton's First Law

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsEngine : MonoBehaviour {
+
+	public Vector3 v;  // Average velocity of this FixedUpdate() loop
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate () {
+		transform.position += v * Time.deltaTime;
+	}
+}
