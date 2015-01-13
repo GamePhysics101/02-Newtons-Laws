730e69f99e81b4c6331e36ee0965b69904a4183c
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-13 15:15:20 +0000

    Summing The Forces

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PhysicsEngine : MonoBehaviour {
 	public Vector3 velocityVector;  // average velocity this FixedUpdate()
+	public Vector3 netForceVector;
+	public List<Vector3> forceVectorList = new List<Vector3>();
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +13,20 @@ public class PhysicsEngine : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-		transform.position += velocityVector * Time.deltaTime;
+		AddForces ();
+		
+		if (netForceVector == Vector3.zero) {
+			transform.position += velocityVector * Time.deltaTime;
+		} else {
+			Debug.LogError ("Unbalanced for detected, help!");
+		}
+	}
+	
+	void AddForces () {
+		netForceVector = Vector3.zero;
+		
+		foreach (Vector3 forceVector in forceVectorList) {
+			netForceVector = netForceVector + forceVector;
+		}
 	}
 }
