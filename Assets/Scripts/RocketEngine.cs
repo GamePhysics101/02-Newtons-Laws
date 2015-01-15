157517ea199bac831e5b4a57b44c9a4dfdcd61f3
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-15 18:43:24 +0000

    Prototyping rocket engine

--- Assets/Scripts/RocketEngine.cs
+++ Assets/Scripts/RocketEngine.cs
@@ -10,13 +10,40 @@ public class RocketEngine : MonoBehaviour {
 	public Vector3 thrustUnitVector;	// [none]
 	
 	private PhysicsEngine physicsEngine;
+	private float currentThrust;		// N  <- Note Newtons NOT kN
 
 	// Use this for initialization
 	void Start () {
 		physicsEngine = GetComponent<PhysicsEngine>();
+		physicsEngine.mass += fuelMass;
 	}
 	
 	void FixedUpdate () {
-		physicsEngine.AddForce (thrustUnitVector);
+		// If there's enough fuel to burn for a full Time.deltaTime
+		if (fuelMass > FuelThisUpdate()) {
+			fuelMass -= FuelThisUpdate();
+			physicsEngine.mass -= FuelThisUpdate();
+			ExertForce ();
+		} else {
+			Debug.LogWarning ("Out of rocket fuel");
+		}
+	}	
+	
+	float FuelThisUpdate () {				// [kg]
+		float exhaustMassFlow;				// [kg s^-1]
+		float effectiveExhastVelocity;		// [m s^-1]
+		
+		effectiveExhastVelocity = 4462f;	// Liquid HO engine
+		
+		// thrust = massFlow * exhaustVelocity
+		// massFlow = thrust / exhaustVelocity
+		exhaustMassFlow = currentThrust / effectiveExhastVelocity;				// [kg]
+		return exhaustMassFlow * Time.deltaTime;
+	}
+	
+	void ExertForce () {
+		currentThrust = thrustPercent * maxThrust * 1000f;
+		Vector3 thrustVector = thrustUnitVector * currentThrust;  // kN
+		physicsEngine.AddForce (thrustVector);
 	}
 }
