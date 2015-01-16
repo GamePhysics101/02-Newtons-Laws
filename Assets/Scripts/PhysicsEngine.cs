bc179630135e6427b06e161a2dd86fbdb7fa3abc
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-16 13:55:09 +0000

    Modelling Gravity

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -8,19 +8,41 @@ public class PhysicsEngine : MonoBehaviour {
 	public Vector3 netForceVector;  // N [kg m s^-2]
 	
 	private List<Vector3> forceVectorList = new List<Vector3>();
+	private PhysicsEngine[] physicsEngineArray;
 
+	private const float bigG = 6.673e-11f; // [m^3 s^-2 kg^-1]
+	
 	// Use this for initialization
 	void Start () {
 		SetupThrustTrails();
+		physicsEngineArray = GameObject.FindObjectsOfType<PhysicsEngine>();
 	}
 	
 	void FixedUpdate () {
 		RenderTrails ();
+		CalculateGravity ();
 		UpdatePosition ();
 	}
 	
 	public void AddForce (Vector3 forceVector) {
 		forceVectorList.Add (forceVector);
+	}
+	
+	void CalculateGravity () {
+		foreach (PhysicsEngine physicsEngineA in physicsEngineArray) {
+			foreach (PhysicsEngine physicsEngineB in physicsEngineArray)	{
+				if (physicsEngineA != physicsEngineB) {
+					Debug.Log (	"Calculating force exerted on " + physicsEngineA.name + 
+								" due to the gravity of " + physicsEngineB.name);
+					Vector3 offset = physicsEngineA.transform.position - physicsEngineB.transform.position;
+					float rSquared = Mathf.Pow (offset.magnitude, 2f);
+					float gravityMagnitude = bigG * physicsEngineA.mass * physicsEngineB.mass / rSquared;
+					Vector3 gravityFeltVector = gravityMagnitude * offset.normalized;
+					
+					physicsEngineA.AddForce (-gravityFeltVector);
+				}
+			}
+		}
 	}
 	
 	void UpdatePosition () {
