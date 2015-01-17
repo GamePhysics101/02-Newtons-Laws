80bd828737f29f08dd8473ae840aab27b0652ab0
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-17 08:35:01 +0000

    Modelling Simple Air Resistance

---
+++ Assets/Scripts/FluidDrag.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluidDrag : MonoBehaviour {
+
+	[Range (1, 2f)]
+	public float velocityExponent;		// [none]
+	public float dragConstant;			// ??
+	
+	private PhysicsEngine physicsEngine;
+	
+	// Use this for initialization
+	void Start () {
+		physicsEngine = GetComponent<PhysicsEngine>();
+	}
+	
+	void FixedUpdate () {
+		Vector3	velocityVector = physicsEngine.velocityVector;
+		float speed = velocityVector.magnitude;
+		float dragSize = CalculateDrag (speed);
+		Vector3 dragVector = dragSize * -velocityVector.normalized;
+		
+		physicsEngine.AddForce (dragVector);
+	}
+
+	float CalculateDrag (float speed) {
+		return dragConstant * Mathf.Pow (speed, velocityExponent);
+	}
+}
