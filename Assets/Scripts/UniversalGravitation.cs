56db3144964e39d2d26ad1b3c6e1fba7411ddc86
Ben Tristem <ben@CompleteUnityDeveloper.com>
2015-01-18 10:21:19 +0000

    Script Execution Order

---
+++ Assets/Scripts/UniversalGravitation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UniversalGravitation : MonoBehaviour {
+	
+	private PhysicsEngine[] physicsEngineArray;
+
+	private const float bigG = 6.673e-11f; // [m^3 s^-2 kg^-1]
+	
+	// Use this for initialization
+	void Start () {
+		physicsEngineArray = GameObject.FindObjectsOfType<PhysicsEngine>();
+	}
+	
+	void FixedUpdate () {
+		CalculateGravity  ();
+	}
+	
+	void CalculateGravity () {
+		foreach (PhysicsEngine physicsEngineA in physicsEngineArray) {
+			foreach (PhysicsEngine physicsEngineB in physicsEngineArray)	{
+				if (physicsEngineA != physicsEngineB && physicsEngineA != this) {
+					Vector3 offset = physicsEngineA.transform.position - physicsEngineB.transform.position;
+					float rSquared = Mathf.Pow (offset.magnitude, 2f);
+					float gravityMagnitude = bigG * physicsEngineA.mass * physicsEngineB.mass / rSquared;
+					Vector3 gravityFeltVector = gravityMagnitude * offset.normalized;
+                    
+                    physicsEngineA.AddForce (-gravityFeltVector);
+                }
+            }
+        }
+    }
+}
